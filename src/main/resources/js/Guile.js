cfd877597d856588e9604d3939e43984343541d6
luke.gumbley <luke.gumbley@gmail.com>
2015-05-16 20:38:36 +1200

    Add jQuery SVG to gadget and render a sample polyline.

--- src/main/resources/js/Guile.js
+++ src/main/resources/js/Guile.js
@@ -1,6 +1,10 @@
 GADGET = {
     template: function (args) {
         var gadget=this;
-        gadget.getView().html("<p>Fiddle faddle foddle!</p>")
+        gadget.getView().svg({onLoad:GADGET.render,settings:{width:'100%',height:'100%'}});
+    },
+
+    render: function(svg) {
+        svg.polyline([[20,20],[40,25],[60,40],[80,120],[120,140],[200,180]], {fill: 'none', stroke: 'black', strokeWidth: 3});
     }
 };
