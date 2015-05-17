42a403973bc9bb8c4790fc36fb2f7f8c51bd6f8e
luke.gumbley <luke.gumbley@gmail.com>
2015-05-17 16:33:17 +1200

    Fix dynamic height dependency to prevent JS errors Add first cut of gadget config section, with dynamic board list Fix oauth and atlassian.util dependencies to permit REST calls Add gadget.common locale for basic widget text

--- src/main/resources/js/Guile.js
+++ src/main/resources/js/Guile.js
@@ -4,6 +4,23 @@ GADGET = {
         gadget.getView().svg({onLoad:GADGET.render,settings:{width:'100%',height:'100%'}});
     },
 
+    descriptor: function(args) {
+        return {
+            //action: "validation url",
+            theme: "long-label", //top-label
+            fields:[{
+                //id: "cumulative-field",
+                //class: "cumulative-select-list",
+                userpref: "board",
+                label: "Board",
+                //description: "Select the board containing the sprint you wish to view.",
+                type: "select",
+                selected: gadget.getPref("board"),
+                options: args.rapidview.views.map(function(board) {return {label:board.name,value:board.id};})
+            }, AJS.gadget.fields.nowConfigured()]
+        };
+    },
+
     render: function(svg) {
         svg.polyline([[20,20],[40,25],[60,40],[80,120],[120,140],[200,180]], {fill: 'none', stroke: 'black', strokeWidth: 3});
     }
