ff08747989299afdb29a5f89f0f13effbe9a5747
luke.gumbley <luke.gumbley@gmail.com>
2015-05-17 22:10:32 +1200

    Add first cut of dynamically populated sprint selector.

--- src/main/resources/js/Guile.js
+++ src/main/resources/js/Guile.js
@@ -1,7 +1,38 @@
 GADGET = {
+    ajax: function(settings) {
+        var deferred = AJS.$.Deferred();
+        settings.success = function() {deferred.resolveWith(this,arguments);};
+        settings.error = function() {deferred.rejectWith(this,arguments);};
+        return deferred.promise(AJS.$.ajax(settings));
+    },
+
     template: function (args) {
         var gadget=this;
         gadget.getView().svg({onLoad:GADGET.render,settings:{width:'100%',height:'100%'}});
+    },
+
+    updateSprintList: function(boardId, sprintFieldId) {
+        var ajax = AJS.$.ajax({
+            url: '/rest/greenhopper/1.0/sprintquery/' + boardId,
+            data: {
+                includeFutureSprints: false
+            },
+            contentType: 'application/json',
+            success: function(xhr) {
+                var selectedSprint = gadget.getPref('sprint');
+
+                var sprintField = AJS.$('#' + sprintFieldId);
+                sprintField.empty();
+
+                sprintField.append(xhr.sprints.map(function(sprint) {
+                    var sprintOption = AJS.$("<option/>").attr("value", sprint.id).text(sprint.name);
+                    if(selectedSprint == sprint.id) {
+                        sprintOption.attr("selected", "selected");
+                    }
+                    return sprintOption[0];
+                }));
+            }
+        });
     },
 
     descriptor: function(args) {
@@ -9,15 +40,35 @@ GADGET = {
             //action: "validation url",
             theme: "long-label", //top-label
             fields:[{
-                //id: "cumulative-field",
-                //class: "cumulative-select-list",
-                userpref: "board",
-                label: "Board",
-                //description: "Select the board containing the sprint you wish to view.",
+                id: "board-select",
+                userpref: 'board',
+                label: 'Board',
                 type: "select",
                 selected: gadget.getPref("board"),
-                options: args.rapidview.views.map(function(board) {return {label:board.name,value:board.id};})
-            }, AJS.gadget.fields.nowConfigured()]
+                //args.rapidview.views
+                options: [{name:'one',id:1},{name:'two',id:2}].map(function(board) {return {label:board.name,value:board.id};})
+            }, {
+                id: 'sprint-parent',
+                userpref: 'sprint',
+                label: 'Sprint',
+                type: "callbackBuilder",
+                callback: function (parentDiv) {
+                    var sprintFieldId='sprint-select';
+
+                    var sprintField = AJS.$("<select>").attr({id:sprintFieldId,name:'sprint'}).addClass('select');
+                    parentDiv.append(sprintField);
+
+                    var boardField = AJS.$('#board-select');
+                    var boardId=boardField.find('option:selected').attr('value');
+                    GADGET.updateSprintList(boardId, sprintFieldId);
+
+                    boardField.change(function(event){
+                        var boardId=AJS.$(event.target).find('option:selected').attr('value');
+                        GADGET.updateSprintList(boardId, sprintFieldId)
+                    });
+               }
+            },
+            AJS.gadget.fields.nowConfigured()]
         };
     },
 
