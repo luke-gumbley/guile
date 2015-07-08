51dfb96f64129f8b26439c63a902bdcced7fd506
luke.gumbley <luke.gumbley@gmail.com>
2015-07-08 22:08:56 +1200

    Another refactoring, this time of dubious benefit.

--- src/main/resources/js/Guile.js
+++ src/main/resources/js/Guile.js
@@ -63,6 +63,7 @@ var guile = (function($) {
 				.filter(function(node) { return node.type === 'SymbolNode'; })
 				.map(function(node) { return node.name; });
 			this.compiled = parsed.compile(math);
+			this.points = [];
 
 			this.line = {
 				fill: 'none',
@@ -79,6 +80,20 @@ var guile = (function($) {
 			}, 0)
 		},
 
+		add: function(x, issues) {
+			var y = this.aggregate(issues);
+			if(y !== this.y) {
+				if(this.y !== undefined) this.points.push([x, this.y]);
+				this.points.push([x, y]);
+				this.y = y;
+				if(this.initial === undefined) this.initial = y;
+			}
+		},
+
+		complete: function(x) {
+			this.points.push([x, this.y]);
+		},
+
 		parse: function(expr) {
 			expr = math.parse(expr);
 
@@ -89,7 +104,34 @@ var guile = (function($) {
 				expr: expr.compile(math)
 			};
 		},
+
+		max: function() {
+			return this.points.reduce(function(max, point) { return max > point[1] ? max : point[1]; }, 0);
+		},
 	});
+
+	guile.processEvents = function(changes, periods, complete) {
+		var events = [];
+		for(field in changes)
+			for(issue in changes[field]) {
+				var flattened = changes[field][issue].map(function(change) {
+					return $.extend({ field: field, issue: issue }, change )
+				});
+				events = events.concat(flattened);
+			}
+
+		// TODO: Combine the sprintData and issueData calls on the backend to allow this filtration on the server
+		return events.filter(function(event) { return event.date <= complete; })
+			.sort(function(a,b) {return a.date - b.date;})
+			.map(function(event) {
+				// parse as a float if possible, math.js can work with strings though so don't rule out crazy stuff.
+				var value = event.value === "" ? 0 : parseFloat(event.value);
+				return $.extend(event, {
+					value: isNaN(value) ? event.value : value,
+					time: periods.calculate(event.date)
+				});
+			});
+	};
 
 	return guile;
 }(AJS.$));
@@ -356,8 +398,6 @@ GADGET = {
 	},
 
 	simulate: function(sprint, plots, events, timeAxis) {
-		var max = undefined;
-
 		// ensure a default is set for every variable required for every plot
 		var defaults = {};
 		plots.forEach(function(plot) {
@@ -368,19 +408,16 @@ GADGET = {
 
 		// start with no knowledge of any issues...
 		var issues = {};
+		var started = false;
 
 		// and build issue knowledge with each event
 		events.forEach(function(event) {
 			// calculate and post the opening total for each plot, once all pre-sprint events have been processed.
-			if(max === undefined && event.date > sprint.start) {
-				max = 0;
+			if(!started && event.date > sprint.start) {
+				started = true;
 				// only issues in the sprint or with a parent in the sprint are relevant
 				var relevant = GADGET.relevant(issues, sprint.id);
-				plots.forEach(function(plot) {
-					plot.initial = plot.y = plot.aggregate(relevant);
-					if(plot.y > max) max = plot.y;
-					plot.points = [[0, plot.y]];
-				});
+				plots.forEach(function(plot) { plot.add(0, relevant); });
 			}
 
 			// update the issue records with information from this event
@@ -391,28 +428,10 @@ GADGET = {
 			// if within the sprint
 			if(event.date > sprint.start) {
 				var relevant = GADGET.relevant(issues, sprint.id);
-
 				var x = event.time[timeAxis];
-
-				plots.forEach(function(plot) {
-					var y = plot.aggregate(relevant);
-					// and a transition occurred
-					if(y !== plot.y) {
-						// plot that transition.
-						plot.points.push([x, plot.y], [x, y]);
-						plot.y = y;
-						if(y > max) max = y;
-					}
-				});
+				plots.forEach(function(plot) { plot.add(x, relevant); });
 			}
 		});
-
-		var x = sprint.periods.calculate(sprint.complete)[timeAxis];
-		plots.forEach(function(plot) {
-			plot.points.push([x, plot.y])
-		});
-
-		return max;
 	},
 
 	calculateIdeal: function(initial, timeAxis, periods) {
@@ -426,7 +445,9 @@ GADGET = {
 
 			points: periods.periods.map(function(period) {
 				return [period[timeAxis], (1 - period.constantWork / periods.totals.constantWork) * initial];
-			}).concat([[ periods.totals[timeAxis], 0 ]])
+			}).concat([[ periods.totals[timeAxis], 0 ]]),
+
+			max: function() { return 0; }
 		};
 	},
 
@@ -436,6 +457,8 @@ GADGET = {
         var ratio = ratioPref[0] / ratioPref[1];
         var width = gadgetDiv.width();
         var height = width / ratio;
+
+		svg.configure({width: width, height: height});
 
 		var temp = args.sprintData.complete - args.sprintData.start;
 		args.sprintData.periods = [{
@@ -452,46 +475,30 @@ GADGET = {
 			rate: 0,
 		}];
 
-		args.sprintData.periods = new guile.Periods(args.sprintData.start, args.sprintData.end, args.sprintData.periods);
-
-		svg.configure({width: width, height: height});
+		var periods = new guile.Periods(args.sprintData.start, args.sprintData.end, args.sprintData.periods);
 
 		var timeAxis = gadget.getPref("timeAxis");
 
 		var plots = GADGET.getPlots().map(function(plot) { return new guile.Plot(plot.expr, plot.colour); });
 
-		// Flatten JSON event hierarchy
-		var events = [];
-		for(field in args.issueData.changes) {
-			for(issue in args.issueData.changes[field]) {
-				args.issueData.changes[field][issue].forEach(function(event) {
-					event.field = field;
-					event.issue = issue;
-					// parse as a float if possible, math.js can work with strings though so don't rule out crazy stuff.
-					var value = event.value === "" ? 0 : parseFloat(event.value);
-					event.value = isNaN(value) ? event.value : value;
-					// TODO: Combine the sprintData and issueData calls on the backend to allow this filtration on the server
-					if(event.date <= args.sprintData.complete) events.push(event);
-				})
-			}
-		}
-		events.sort(function(a,b) {return a.date - b.date;});
+		var events = guile.processEvents(args.issueData.changes, periods, args.sprintData.complete);
 
-		events.forEach(function(event) { event.time = args.sprintData.periods.calculate(event.date); });
+		GADGET.simulate(args.sprintData, plots, events, timeAxis);
 
-		var max = GADGET.simulate(args.sprintData, plots, events, timeAxis);
+		var x = periods.calculate(args.sprintData.complete)[timeAxis];
+		plots.forEach(function(plot) { plot.complete(x); })
 
 		var idealPlot = gadget.getPref('idealPlot');
 		if(idealPlot !== '')
-			plots.unshift(GADGET.calculateIdeal(plots[idealPlot].initial, timeAxis, args.sprintData.periods));
+			plots.unshift(GADGET.calculateIdeal(plots[idealPlot].initial, timeAxis, periods));
 
 		var left = 20;
 		var top = 20;
 		var right = width - 20;
 		var bottom = height - 20;
 
-		var xScale = (right - left) / args.sprintData.periods.totals[timeAxis];
-		var yScale = (top - bottom) / max;
+		var xScale = (right - left) / periods.totals[timeAxis];
+		var yScale = (top - bottom) / Math.max.apply(Math, plots.map(function(plot) { return plot.max(); }));
 
 		plots.forEach(function(plot) {
 			plot.points.forEach(function(point) {
@@ -505,13 +512,13 @@ GADGET = {
 		svg.polyline([[left, top], [left, bottom], [right, bottom]],{fill:'none', stroke:'grey', strokeWidth:1 });
 
 		// timeAxis, args.sprintData.start, args.sprintData.end
-		// args.sprintData.periods,
+		// periods,
 		// width, xScale, left, top, bottom
 
 		// Maximum number of intervals, assuming a 50-pixel gap
 		var maxIntervals = (width - 40) / 50;
 		// Minimum gap between intervals given the amount of time rendered on the graph
-		var minInterval = args.sprintData.periods.totals[timeAxis] / maxIntervals / 60000;
+		var minInterval = periods.totals[timeAxis] / maxIntervals / 60000;
 
 		// Round the time interval up to something sensible
 		var hour=60, day=24*hour;
@@ -527,7 +534,7 @@ GADGET = {
 		// Treat interval as real-time.
 		var line, text;
 		for(var time = args.sprintData.start + offset; time <= args.sprintData.end; time += interval * 60000) {
-			var sX = (args.sprintData.periods.calculate(time)[timeAxis] * xScale + left).toFixed(3);
+			var sX = (periods.calculate(time)[timeAxis] * xScale + left).toFixed(3);
 			if(!line || line.attr('x1') !== sX) {
 				line = AJS.$(svg.line(sX, top, sX, bottom + 3, { stroke: 'grey', 'stroke-width': '0' }));
 				if(!text || Number.parseFloat(text.attr('x')) < Number.parseFloat(sX) - 50)
